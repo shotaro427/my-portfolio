e9c1ea4ad97f61bb32b4ea02c89a8b42da0506a5
shotaro427 <toshinobu.s111@gmail.com>
2021-09-14 08:20:02 +0900

    modify about section

--- src/components/organisms/AboutSection.tsx
+++ src/components/organisms/AboutSection.tsx
@@ -8,18 +8,18 @@ const AboutSection: React.FC = () => {
   return (
     <Background>
       <Section>
-        <StaticImage style={selfImageStyles} width={350} height={350} src="../../images/selfimage.png" alt="selfimage" />
-        <Content>
-          <SectionTitle styles={{ marginBottom: 0 }} text="ABOUT" textAlign="left" />
-          <AboutBoldText>田内 翔太郎</AboutBoldText>
-          <AboutText>
-            京都在住のフリーランスエンジニア。
-            <br />
-            Web/モバイル/バックエンド/インフラ設計・構築など多岐にわたって活動中。
-            <br />
-            また、個人的にブログや開発したサービスを運営したりもしています。
-          </AboutText>
-        </Content>
+        <SectionContent>
+          <StaticImage style={selfImageStyles} width={350} height={350} src="../../images/selfimage.png" alt="selfimage" />
+          <Content>
+            <SectionTitle styles={{ marginBottom: 0, marginTop: 0 }} text="ABOUT" textAlign="left" />
+            <AboutBoldText>田内 翔太郎</AboutBoldText>
+            <AboutText>
+              京都在住のフリーランスエンジニア。
+              <br />
+              Web/モバイル/バックエンド/インフラ設計・構築など多岐にわたって活動中。
+            </AboutText>
+          </Content>
+        </SectionContent>
       </Section>
     </Background>
   )
@@ -28,50 +28,57 @@ const AboutSection: React.FC = () => {
 const AboutBoldText = styled.p`
   margin: 0;
   font-family: ${constant.font.family.DIN2014};
-  font-size: ${constant.font.size.small};
+  font-size: ${constant.font.size.xsmall};
   font-weight: bold;
 `
 
 const AboutText = styled.p`
   margin: 0;
   font-family: ${constant.font.family.DIN2014};
-  font-size: ${constant.font.size.small};
+  font-size: ${constant.font.size.xsmall};
 `
 
 const Background = styled.div`
   display: flex;
-  align-items: center;
-  width: 70%;
+  align-items: flex-start;
+  width: 100vw;
+  height: 45vw;
   margin: auto;
-  background-image: url('/static/0685722d208b6591a93329fbdc3a06ad/about-image.svg');
+  background-image: url('/static/24750f5b30f27085e79d0e207b0e16cb/about-image.svg');
   background-repeat: no-repeat;
   background-position: center;
+  background-size: contain;
+`
+
+const SectionContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 48px;
 `
 
 const Section = styled.section`
   position: relative;
   display: flex;
-  width: 100%;
-  height: 100%;
-  flex-direction: row;
+  max-width: 60%;
   justify-content: center;
+  align-items: center;
   margin: auto;
+  backdrop-filter: blur(15px);
+  -webkit-backdrop-filter: blur(15px);
   background: ${constant.color.white_a40};
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(20px);
-  -webkit-backdrop-filter: blur(20px);
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
 `
 
 const Content = styled.div`
-  width: 50%;
-  margin: 0 32px;
+  width: 60%;
 `
 
 const selfImageStyles: React.CSSProperties = {
   borderRadius: '10px',
-  margin: '32px 40px 32px 32px',
+  marginRight: '48px',
   border: '4px solid transparent'
 }
 
